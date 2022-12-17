4211415e7877cacd6a6a8bc7797871a4fb168b83
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2022-12-17 12:50:30 -0800

    send email functionality

--- base/views.py
+++ base/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
 
+from .forms import ContactForm
 
 def index(request):
     return render(request, 'base/index.html')
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            content = form.cleaned_data['content']
+
+            html = render_to_string('base/emails/contactform.html', {
+                'name':name,
+                'email':email,
+                'content':content
+            })
+
+            send_mail('The contact form subject', 'This is the message', 'noreply@code.com', ['cesarjaramillodev@gmail.com'], html_message=html)
+
+            return redirect('/')
+    else:
+        print('error')
+        form = ContactForm()
+    
+    return render(request, 'base/contact.html', {'form':form})
--- paulwebsite/settings.py
+++ paulwebsite/settings.py
@@ -27,6 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = 'cesarjaramillodev@gmail.com'
+EMAIL_HOST_PASSWORD = 'jetscpdrcxkymjcn'
+EMAIL_USE_TLS = True
 
 # Application definition
 

d6a587f8aa650ffe3cdaed36a32736c6d7a5d816
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2022-12-18 18:31:30 -0800

    basic form skeleton

--- base/forms.py
+++ base/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100)
-    email = forms.EmailField()
-    content = forms.CharField(widget=forms.Textarea)
+    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Name', 'style': 'width: 300px;', 'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Email', 'style': 'width: 300px;', 'class': 'form-control'}))
+    content = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Subject', 'style': 'width: 300px;', 'class': 'form-control'}))

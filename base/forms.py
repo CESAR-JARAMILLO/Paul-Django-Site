17256e9493f15f0d68b3321e4b84f2c58511ed87
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2022-12-21 11:38:38 -0800

    small edits

--- base/forms.py
+++ base/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Name', 'class': 'form-control'}))
-    email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Email', 'class': 'form-control'}))
-    content = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Subject', 'class': 'form-control'}))
+    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    subject = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
--- base/views.py
+++ base/views.py
@@ -14,19 +14,18 @@ def contact(request):
         if form.is_valid():
             name = form.cleaned_data['name']
             email = form.cleaned_data['email']
-            content = form.cleaned_data['content']
+            subject = form.cleaned_data['subject']
 
             html = render_to_string('base/emails/contactform.html', {
                 'name':name,
                 'email':email,
-                'content':content
+                'subject':subject
             })
 
             send_mail('The contact form subject', 'This is the message', 'noreply@code.com', ['cesarjaramillodev@gmail.com'], html_message=html)
 
             return redirect('/')
     else:
-        print('error')
         form = ContactForm()
     
     return render(request, 'base/contact.html', {'form':form})

34bfe5088146ec099854283055388cfe003f1fa2
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2022-12-21 12:03:53 -0800

    config vars test

--- paulwebsite/settings.py
+++ paulwebsite/settings.py
@@ -14,6 +14,8 @@ import os
 
 from pathlib import Path
 
+s3 = os.environ.get('EMAIL_PASSWORD')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -33,7 +35,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = 'cesarjaramillodev@gmail.com'
-EMAIL_HOST_PASSWORD = 'jetscpdrcxkymjcn'
+EMAIL_HOST_PASSWORD = s3
 EMAIL_USE_TLS = True
 
 # Application definition
